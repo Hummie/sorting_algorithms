8c15bbb416e4b909d948467424254ca4bdb82066
Hummie <humphreywill77@gmail.com>
2023-10-24 08:35:19 -0700

    Additions

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort list with selection_sort
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * selection_sort - sort list with selection_sort.
+ * @array: The array to be printed.
+ * @size: Number of elements in @array.
  */
 void selection_sort(int *array, size_t size)
 {
