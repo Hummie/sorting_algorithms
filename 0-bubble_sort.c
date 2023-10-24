737adbdaa641d8454991c98a3977486403a150bf
Hummie <humphreywill77@gmail.com>
2023-10-24 08:33:33 -0700

    Change

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,10 +2,10 @@
 
 /**
   * bubble_sort - sorts an array of integers in ascending order
-  * using the Bubble sort algorithm
-  * @array: pointer to an array of type int
-  * @size: size of the array
-  * Return: Always Nothing
+  * using the Bubble sort algorithm.
+  * @array: pointer to an array of type int.
+  * @size: size of the array.
+  * Return: Nothing
   **/
 
 void bubble_sort(int *array, size_t size)
